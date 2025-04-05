b3be82aadb04f6a9dd442c4cdf03d456b873811f
Omkar Narveer <omkarnarveer07@gmail.com>
2025-04-06 03:52:57 +0530

    Production build

--- app.py
+++ app.py
@@ -56,3 +56,5 @@ if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     os.makedirs('static/results', exist_ok=True)
     app.run(debug=False)
+    port = int(os.environ.get('PORT', 10000))  # Use Render's PORT or default to 5000
+    app.run(host='0.0.0.0', port=port)
